0bd075f2934b9887c6f6c1574c8184e0b2626df9
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-04-27 21:59:52 +0300

    last

--- week01/js/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('copyrightYear').innerHTML = new Date().getFullYear();
-document.getElementById("modify").textContent = document.lastModified;
-
-const links = [
-  {
-    label: "Week1 notes",
-    url: "week1/index.html"
-  }
-]
